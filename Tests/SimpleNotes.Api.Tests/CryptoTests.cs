960981bc1687d3f30bb9035b76790f2ff116f660
Cameron Johnson-Brown <camjohnsbrown@gmail.com>
2020-02-01 15:26:25 -0500

    Remove separate cryptography project

--- Tests/SimpleNotes.Api.Tests/CryptoTests.cs
+++ Tests/SimpleNotes.Api.Tests/CryptoTests.cs
@@ -1,25 +1,26 @@
-﻿using System;
-using SimpleNotes.Cryptography;
+﻿using SimpleNotes.Api.Services;
 using Xunit;
 
-namespace SimpleNotes.Cryptography.Tests {
+namespace SimpleNotes.Api.Tests {
     public class CryptoTests {
 
         [Fact]
         public void DeriveKeyForPassowrd() {
+            CryptoService crypto = new CryptoService();
             string password = "Password_1";
             string key = "/aExOhfLY8QV11tghX5NpRXydJUyIu6kPWwO+mwyMnU=";
-            string output = Crypto.DeriveKey(password);
+            string output = crypto.DeriveKey(password);
             Assert.Equal(output, key);
 
         }
 
         [Fact]
         public void EncryptDecrypt() {
+            CryptoService crypto = new CryptoService();
             string key = "aLYGMYOcgI5WK/X8PAp8L6K/xcH3tj5vbIYcvdhoOtw=";
             string plainText = "Encrypt this string, please";
-            string cipherText = Crypto.Encrypt(key,plainText);
-            string output = Crypto.Decrypt(key, cipherText);
+            string cipherText = crypto.Encrypt(key,plainText);
+            string output = crypto.Decrypt(key, cipherText);
             Assert.Equal(output, plainText);
         }
 
